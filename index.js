c8afed9c33ac2a92002855adae599d4df66da431
Calvin Chege <calvinchege1@gmail.com>
2024-04-09 20:32:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const change=document.getElementById("text")
+    change.textContent="This is really cool!"
+    console.log(change)
+  });
